3e941d48bf190701b39d18a41bd112f1fb87b7d9
James Swatton <jamessswatton@gmail.com>
2019-04-03 15:44:03 +0100

    add sum_arry

--- start_point/my_functions.rb
+++ start_point/my_functions.rb
@@ -1,3 +1,11 @@
 def add_array_lengths(array1, array2)
   return array1.length + array2.length
 end
+
+def sum_array(numbers)
+  total = 0
+  for i in numbers
+    total += i
+  end
+  return total
+end
